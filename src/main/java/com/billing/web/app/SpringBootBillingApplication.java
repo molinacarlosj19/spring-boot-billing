f13f9446e2218f659444ac72f251343fcfb29709
Carlos Molina <molinacarlosj@gmail.com>
2023-03-11 06:18:30 -0500

    Base line

---
+++ src/main/java/com/billing/web/app/SpringBootBillingApplication.java
@@ -0,0 +1,13 @@
+package com.billing.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBillingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/billing/web/app/models/entity/Client.java
@@ -0,0 +1,77 @@
+package com.billing.web.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name="clients")
+public class Client implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name="last_name", nullable = false)
+	private String lastName;
+	
+	private String email;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	private static final long serialVersionUID = 1L;
+	
+}
